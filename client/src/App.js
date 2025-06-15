63e584b8b011b1a749b985640d71b4b5a5068732
Telmen Bayarbaatar <156386842+TelmenBay@users.noreply.github.com>
2025-06-15 19:24:45 -0400

    createEvent functionality
    Able to create an event to calendar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { useSession, useSupabaseClient, useSessionContext } from '@supabase/auth-helpers-react';
+import DateTimePicker from 'react-datetime-picker';
+import { useState } from 'react';
+import 'react-datetime-picker/dist/DateTimePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import 'react-clock/dist/Clock.css';
 
 
 function App() {
+  const [ start, setStart] = useState(new Date());
+  const [ end, setEnd] = useState(new Date());
+  const [ eventName, setEventName] = useState("");
+  const [ eventDescription, setEventDescription] = useState("");
+
 
   const session = useSession(); // tokens, when session exists then we have a user
   const supabase = useSupabaseClient(); // talk to supabase
@@ -25,12 +35,44 @@ function App() {
       console.log(error);
     }
   };
+  
 
   async function signOut() {
     await supabase.auth.signOut();
   }
 
+  async function createEvent() {
+    
+    const event = {
+      'summary': eventName,
+      'description': eventDescription,
+      'start': {
+        'dateTime': start.toISOString(),
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+      },
+      'end': {
+        'dateTime': end.toISOString(),
+        'timeZone': Intl.DateTimeFormat().resolvedOptions().timeZone
+      },
+    }
+      await fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        method: 'POST',
+        headers: {
+          'Authorization': 'Bearer ' + session.provider_token // google token
+        },
+        body: JSON.stringify(event)
+      }).then((data) => {
+        return data.json();
+      }).then((data) => {
+        console.log(data);
+        alert("Event created successfully");
+      })
+      
+    
+  }
+
   console.log(session);
+  console.log(start);
 
   return (
     <div className="App">
@@ -38,6 +80,17 @@ function App() {
         {session ? 
           <>
             <h2>Hello {session.user.email}</h2>
+            <p>Start of your event</p>
+            <DateTimePicker onChange={setStart} value={start}/>
+            <p>End of your event</p>
+            <DateTimePicker onChange={setEnd} value={end}/>
+            <p>Name of your event</p>
+            <input type='text' onChange={(e) => setEventName(e.target.value)}/>
+            <p>Description of your event</p>
+            <input type='text' onChange={(e) => setEventDescription(e.target.value)}/>
+            <hr/>
+            <button onClick={() => createEvent()}>Create Event</button>
+            <br/><br/>
             <button onClick={() => signOut()}>Sign Out</button>
           </>
           :
